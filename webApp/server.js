77b519508fa7cee2495565d363101bc3eb6a13d4
riteshwadhwani <riteshwadhwani05@gmail.com>
2025-01-08 09:10:08 +0530

    added product rest api

--- webApp/server.js
+++ webApp/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 console.log("hello world");
 var app = express();
+
+app.use(express.json());
 app.get("/api/hello", function(req, res) {
     var product  = {
         id: 1,
@@ -10,6 +12,27 @@ app.get("/api/hello", function(req, res) {
     }
     res.send(product);
     
+});
+
+app.post("/api/addproduct", function (req, res) {
+  const data = req.body
+  try {
+    console.log(data);
+    productRepo.addProduct(data);
+    res.status(201).send("Product added")
+  } catch (err) {
+    console.log(err)
+  }
+});
+app.get("/api/getbyid/:id", function (req, res) {
+  var data = req.params.id
+  try {
+    var product1 = productRepo.getProductById(data)
+    console.log(product1);
+    res.status(200).send(product1);
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 app.listen(3000, function() {
