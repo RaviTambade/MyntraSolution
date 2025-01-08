ee52fcbd830f88da02f38fd5e463dc8f46ce03f0
riteshwadhwani <riteshwadhwani05@gmail.com>
2025-01-08 09:11:12 +0530

    added

--- webApp/server.js
+++ webApp/server.js
@@ -22,6 +22,27 @@ app.get("/api/thankyou", function(req, res) {
     
 });
 
+app.post("/api/addproduct", function (req, res) {
+  const data = req.body
+  try {
+    console.log(data);
+    productRepo.addProduct(data);
+    res.status(201).send("Product added")
+  } catch (err) {
+    console.log(err)
+  }
+});
+app.get("/api/getbyid/:id", function (req, res) {
+  var data = req.params.id
+  try {
+    var product1 = productRepo.getProductById(data)
+    console.log(product1);
+    res.status(200).send(product1);
+  } catch (err) {
+    console.log(err)
+  }
+});
+
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
